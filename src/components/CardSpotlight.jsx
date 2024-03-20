e45dfc6cee9036fa86f2cc142018a26eb75aab5c
eze stom <ezequielstom@gmail.com>
2024-03-20 17:01:13 -0300

    Fix navigation links in AppHeader and import statement in CardSpotlight

--- src/components/CardSpotlight.jsx
+++ src/components/CardSpotlight.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import  { useRef, useState } from "react";
 
 const CardSpotlight = ({ header, main, footer, img }) => {
 	const divRef = useRef(null);
